c6bfa15ebb6e7874529f4d071a16cf8f62f860f6
priyanshu-gupta <priyanshuguptaofcl@gmail.com>
2017-07-04 01:27:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/priyanshugupta/indorecourierapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.priyanshugupta.indorecourierapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by Priyanshu Gupta on 7/4/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    private Button buttonCustomer, buttonDriver, buttonAdmin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonCustomer = (Button)findViewById(R.id.buttonCustomer);
+        buttonDriver = (Button)findViewById(R.id.buttonDriver);
+        buttonAdmin = (Button)findViewById(R.id.buttonAdmin);
+
+        buttonAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Please Select Other Options", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
