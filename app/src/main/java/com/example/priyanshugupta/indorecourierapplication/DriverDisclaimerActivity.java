8a134cb1ee7c50dbe51ad5d96602f585495ce504
SHUBHAM JAIN <jain.shubhamm12@gmail.com>
2017-07-06 21:23:55 +0530

    XML DRIVER

--- app/src/main/java/com/example/priyanshugupta/indorecourierapplication/DriverDisclaimerActivity.java
+++ app/src/main/java/com/example/priyanshugupta/indorecourierapplication/DriverDisclaimerActivity.java
@@ -1,8 +1,13 @@
 package com.example.priyanshugupta.indorecourierapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by SHUBHAM JAIN on 06-Jul-17.
@@ -10,9 +15,34 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DriverDisclaimerActivity extends AppCompatActivity {
 
+    private TextView disclaimerTv;
+    private Button iagreeBt,idisagreeBt;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disclaimer);
+
+        disclaimerTv =(TextView)findViewById(R.id.disclaimerTv);
+        iagreeBt =(Button)findViewById(R.id.iAgreeBt);
+        idisagreeBt=(Button)findViewById(R.id.iDisagreeAgreeBt);
+
+
+        idisagreeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DriverDisclaimerActivity.this, "Please agree only then we can proceed further", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        iagreeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DriverDisclaimerActivity.this, "Thanks for accepting our agreement ", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(DriverDisclaimerActivity.this,DriverHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
