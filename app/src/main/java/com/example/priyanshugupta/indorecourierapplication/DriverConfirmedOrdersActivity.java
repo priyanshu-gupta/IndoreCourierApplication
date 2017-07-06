c51af4511212d17d302cfd4f43746bdfcc047e01
SHUBHAM JAIN <jain.shubhamm12@gmail.com>
2017-07-06 21:38:26 +0530

    XML DRIVER

---
+++ app/src/main/java/com/example/priyanshugupta/indorecourierapplication/DriverConfirmedOrdersActivity.java
@@ -0,0 +1,18 @@
+package com.example.priyanshugupta.indorecourierapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SHUBHAM JAIN on 06-Jul-17.
+ */
+
+public class DriverConfirmedOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_confirmed_orders);
+    }
+}
